f56dd96076f34dceab337aec52992f7ef9d4489f
Gwan Seok Yi <gooreumgil47@gmail.com>
2023-06-26 11:59:39 +0900

    first commit

---
+++ src/main/java/hello/springtxstart/SpringtxStartApplication.java
@@ -0,0 +1,13 @@
+package hello.springtxstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtxStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringtxStartApplication.class, args);
+    }
+
+}
