69e29084ca9bb8a554005734e0262f8e94fa94f8
gooreumgil <gooreumgil47@gmail.com>
2023-07-06 13:40:54 +0900

    transaction 전파

---
+++ src/main/java/hello/springtxstart/propagation/Member.java
@@ -0,0 +1,27 @@
+package hello.springtxstart.propagation;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+
+    public Member() {
+    }
+
+    public Member(String username) {
+        this.username = username;
+    }
+}
